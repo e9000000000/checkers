f93bcda2b9e9f8853d325bfbb77972e5973dff46
e <skipper224483@gmail.com>
2024-02-06 05:59:18 +0300

    before including egui

--- src/player_minmax.rs
+++ src/player_minmax.rs
@@ -4,31 +4,31 @@ use crate::board::{Board, Move, Side, Cell};
 #[derive(Debug)]
 struct ScoredMove {
     mv: Move,
-    score: f64,
+    score: i8,
 }
 
 /// positive numbers is good position, negative numbers is bad position
-fn count_score(board: &Board, side: Side, king_multiplier: f64) -> f64 {
+fn count_score(board: &Board, side: Side, king_multiplier: i8) -> i8 {
     match side {
         Side::Black => {
-            let our_checkers_amount = board.count(Cell::Black) as f64;
-            let our_kings_amount = board.count(Cell::BlackKing) as f64;
-            let enemy_checkers_amount = board.count(Cell::White) as f64;
-            let enemy_kings_amount = board.count(Cell::WhiteKing) as f64;
+            let our_checkers_amount = board.count(Cell::Black) as i8;
+            let our_kings_amount = board.count(Cell::BlackKing) as i8;
+            let enemy_checkers_amount = board.count(Cell::White) as i8;
+            let enemy_kings_amount = board.count(Cell::WhiteKing) as i8;
             return (our_checkers_amount + our_kings_amount * king_multiplier) - (enemy_checkers_amount + enemy_kings_amount * king_multiplier);
         },
         Side::White => {
-            let our_checkers_amount = board.count(Cell::White) as f64;
-            let our_kings_amount = board.count(Cell::WhiteKing) as f64;
-            let enemy_checkers_amount = board.count(Cell::Black) as f64;
-            let enemy_kings_amount = board.count(Cell::BlackKing) as f64;
+            let our_checkers_amount = board.count(Cell::White) as i8;
+            let our_kings_amount = board.count(Cell::WhiteKing) as i8;
+            let enemy_checkers_amount = board.count(Cell::Black) as i8;
+            let enemy_kings_amount = board.count(Cell::BlackKing) as i8;
             return (our_checkers_amount + our_kings_amount * king_multiplier) - (enemy_checkers_amount + enemy_kings_amount * king_multiplier);
         },
     }
 }
 
 
-fn moves_with_scores(board: &Board, depth: usize, king_multiplier: f64) -> Vec<ScoredMove> {
+fn moves_with_scores(board: &Board, depth: usize, king_multiplier: i8) -> Vec<ScoredMove> {
     let mut scored_moves = vec![];
     let mvs = board.all_awailable_moves();
     if mvs.len() == 0 {
@@ -40,14 +40,18 @@ fn moves_with_scores(board: &Board, depth: usize, king_multiplier: f64) -> Vec<S
         let mut test_board = board.clone();
         test_board.do_move(mv).unwrap();
         let score = match test_board.who_win() {
-            Some(x) if x == board.who_turn() => 9999.,
-            Some(x) if x != board.who_turn() => -9999.,
+            Some(x) if x == board.who_turn() => 127,
+            Some(x) if x != board.who_turn() => -128,
             None => {
                 match depth {
                     0 => count_score(&test_board, board.who_turn(), king_multiplier),
                     _ => {
-                        let enemy_moves = moves_with_scores(&test_board, depth - 1, king_multiplier);
-                        let mut best_enemy_move_score = -999.;
+                        let new_depth = match mvs.len() {
+                            1 => depth,
+                            _ => depth - 1,
+                        };
+                        let enemy_moves = moves_with_scores(&test_board, new_depth, king_multiplier);
+                        let mut best_enemy_move_score = -128;
                         for j in 0..enemy_moves.len() {
                             if enemy_moves[j].score > best_enemy_move_score {
                                 best_enemy_move_score = enemy_moves[j].score;
@@ -55,7 +59,7 @@ fn moves_with_scores(board: &Board, depth: usize, king_multiplier: f64) -> Vec<S
                         }
                         match test_board.who_turn() == board.who_turn() {
                             true => best_enemy_move_score,
-                            false => best_enemy_move_score * -1.,
+                            false => best_enemy_move_score * -1,
                         }
                     }
                 }
@@ -70,13 +74,13 @@ fn moves_with_scores(board: &Board, depth: usize, king_multiplier: f64) -> Vec<S
 
 
 /// dir can be -1 or 1 it is for best or words move chousing (1 for best, -1 for worst)
-fn best_move(board: &Board, depth: usize, king_multiplier: f64) -> Option<Move> {
+fn best_move(board: &Board, depth: usize, king_multiplier: i8) -> Option<Move> {
     let scored_moves = moves_with_scores(&board, depth, king_multiplier);
     if scored_moves.len() == 0 {
         return None;
     } else {
         let mut best_move_index = 0;
-        let mut best_move_score = -9999.;
+        let mut best_move_score = -128;
         for i in 0..scored_moves.len() {
             if scored_moves[i].score > best_move_score {
                 best_move_score = scored_moves[i].score;
@@ -88,7 +92,7 @@ fn best_move(board: &Board, depth: usize, king_multiplier: f64) -> Option<Move> 
 }
 
 pub fn chouse_move(board: &Board) -> Option<Move> {
-    return best_move(board, 5, 3.);
+    return best_move(board, 5, 3);
 }
 
 
@@ -110,7 +114,7 @@ mod tests {
             [' ', 'w', ' ', ' ', ' ', ' ', ' ', ' '],
             [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
         ]);
-        assert_eq!(count_score(&board, Side::White, 33.), 1.);
+        assert_eq!(count_score(&board, Side::White, 33), 1);
     }
 
     #[test]
@@ -125,7 +129,7 @@ mod tests {
             [' ', 'w', ' ', ' ', ' ', ' ', ' ', ' '],
             [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
         ]);
-        assert_eq!(count_score(&board, Side::Black, 33.), -1.);
+        assert_eq!(count_score(&board, Side::Black, 33), -1);
     }
 
     #[test]
@@ -140,7 +144,7 @@ mod tests {
             [' ', 'w', ' ', ' ', ' ', ' ', ' ', ' '],
             [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
         ]);
-        assert_eq!(best_move(&board, 2, 1.), Some(Move::new(5, 2, 4, 1)))
+        assert_eq!(best_move(&board, 2, 1), Some(Move::new(5, 2, 4, 1)))
     }
 
 
@@ -156,7 +160,7 @@ mod tests {
             [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
             [' ', ' ', ' ', ' ', ' ', ' ', 'B', ' '],
         ]);
-        assert_ne!(best_move(&board, 3, 1.), Some(Move::new(6, 7, 5, 6)))
+        assert_ne!(best_move(&board, 3, 1), Some(Move::new(6, 7, 5, 6)))
     }
 
     #[test]
@@ -171,6 +175,6 @@ mod tests {
             [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
             [' ', ' ', ' ', ' ', ' ', ' ', 'W', ' '],
         ]);
-        assert_ne!(best_move(&board, 3, 1.), Some(Move::new(6, 7, 5, 6)))
+        assert_ne!(best_move(&board, 3, 1), Some(Move::new(6, 7, 5, 6)))
     }
 }
