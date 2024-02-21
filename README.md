c8a7579e261c025d0da8ccc6f9e6c191c378c1af
e <skipper224483@gmail.com>
2024-02-21 21:02:05 +0300

    algorithm fixed

--- README.md
+++ README.md
@@ -1,12 +1,12 @@
-* Checkers
-[[./game.png]]
+# Checkers
+![](./game.png)
 
-* how to run
-#+BEGIN_SRC bash
+# how to run
+```bash
 cargo run
-#+END_SRC
+```
 
-* todo
+# todo
 - [X] highlight pieces, which can be sellected to move
 - [X] player will play againts min max algorithm
 - [X] add ability to change side (black/white)
@@ -17,7 +17,7 @@ cargo run
 - [X] optimize minmax algorth (now it plays with acceptable speed when depth <= 5)
 - [X] optimize minmax algorth (now it plays with acceptable speed when depth <= 10)
   - [X] alpha-beta [[https://www.youtube.com/watch?v=l-hh51ncgDI]]
-- [ ] fix algorighm, now it is extrimily stupid, can't win me on depth=20
-- [ ] optimize minmax algorth (now it plays with acceptable speed when depth <= 20)
+- [X] fix algorighm, now it is extrimily stupid, can't win me on depth=20
+- [ ] optimize minmax algorthm (now it plays with acceptable speed when depth <= 16)
   - [ ] multithreading
 - [ ] highlight previous moves, for multijump moves highlight all previous positions
--- src/player_minmax.rs
+++ src/player_minmax.rs
@@ -9,42 +9,31 @@ struct ScoredMove {
 
 /// positive numbers is good position, negative numbers is bad position
 #[inline(always)]
-fn count_score(board: &Board, side: Side) -> i8 {
+fn count_score(board: &Board) -> i8 {
     let who_turn = board.who_turn();
 
     match board.who_win() {
         Some(x) if x == who_turn => 100,
         Some(x) if x != who_turn => -100,
-        None => match side {
-            Side::Black => {
-                let our_checkers_amount = board.count(Cell::Black) as i8;
-                let enemy_checkers_amount = board.count(Cell::White) as i8;
-                let our_king_amount = board.count(Cell::BlackKing) as i8;
-                let enemy_king_amount = board.count(Cell::WhiteKing) as i8;
-                return our_checkers_amount + (our_king_amount * 3) - enemy_checkers_amount - (enemy_king_amount * 3);
-            },
-            Side::White => {
-                let our_checkers_amount = board.count(Cell::White) as i8;
-                let enemy_checkers_amount = board.count(Cell::Black) as i8;
-                let our_king_amount = board.count(Cell::WhiteKing) as i8;
-                let enemy_king_amount = board.count(Cell::BlackKing) as i8;
-                return our_checkers_amount + (our_king_amount * 3) - enemy_checkers_amount - (enemy_king_amount * 3);
-            },
-        },
+        None => (board.count(Cell::White) + board.count(Cell::WhiteKing)*3 - board.count(Cell::Black) - board.count(Cell::BlackKing)*3) as i8,
         Some(_) => unreachable!(),
     }
 }
 
 
-fn compute_best_move(board: &mut Board, depth: usize, start_score: i8, incoming_alpha: i8, incoming_beta: i8) -> ScoredMove {
-    let board_score = count_score(&board, board.who_turn());
+fn compute_best_move(board: &mut Board, depth: usize, incoming_alpha: i8, incoming_beta: i8, play_as_white: bool) -> ScoredMove {
+    let board_score = count_score(&board);
     if depth == 0 {
         return ScoredMove {mv: None, score: board_score};
     }
 
     let mut best_mv = ScoredMove {
         mv: None,
-        score: -127
+        score: if play_as_white {
+            -127
+        } else {
+            127
+        },
     };
 
     let mvs = board.all_available_moves();
@@ -55,35 +44,32 @@ fn compute_best_move(board: &mut Board, depth: usize, start_score: i8, incoming_
 
     let mut alpha = incoming_alpha;
     let mut beta = incoming_beta;
-
+ 
     for i in 0..mvs_amount {
         let mv = mvs[i];
         let mut test_board = board.clone();
         test_board.do_move_without_checks(mv);
 
+        let next_best_move;
         if test_board.who_turn() == board.who_turn() {
-            let best_our_next_move = compute_best_move(&mut test_board, depth - 1, start_score, alpha, beta);
-
-            if best_our_next_move.score > best_mv.score {
-                best_mv.score = best_our_next_move.score;
-                best_mv.mv = Some(mv);
-            }
-
-            if best_our_next_move.score > alpha {
-                alpha = best_our_next_move.score;
-            }
+            next_best_move = compute_best_move(&mut test_board, depth - 1, alpha, beta, play_as_white);
         } else {
-            let best_enemy_move = compute_best_move(&mut test_board, depth - 1, start_score, beta, alpha);
-            let score = best_enemy_move.score * -1;
-
-            if score > best_mv.score {
-                best_mv.score = score;
-                best_mv.mv = Some(mv);
+            next_best_move = compute_best_move(&mut test_board, depth - 1, alpha, beta, !play_as_white);
+        
+            if play_as_white {
+                if next_best_move.score > alpha {
+                    alpha = next_best_move.score;
+                }
+            } else {
+                if next_best_move.score < beta {
+                    beta = next_best_move.score;
+                }
             }
+        }
 
-            if best_enemy_move.score > beta {
-                beta = best_enemy_move.score;
-            }
+        if (play_as_white && next_best_move.score > best_mv.score) || (!play_as_white && next_best_move.score < best_mv.score) {
+            best_mv.score = next_best_move.score;
+            best_mv.mv = Some(mv);
         }
 
         if beta <= alpha {
@@ -97,8 +83,10 @@ fn compute_best_move(board: &mut Board, depth: usize, start_score: i8, incoming_
 
 /// dir can be -1 or 1 it is for best or words move chousing (1 for best, -1 for worst)
 pub fn best_move(board: &mut Board, depth: usize) -> Option<Move> {
-    let start_score = count_score(&board, board.who_turn());
-    return compute_best_move(board, depth, start_score, -127, 127).mv;
+    match board.who_turn() {
+        Side::White => return compute_best_move(board, depth, -127, 127, true).mv,
+        Side::Black => return compute_best_move(board, depth, -127, 127, false).mv,
+    }
 }
 
 pub fn chouse_move5(board: &mut Board) -> Option<Move> {
@@ -110,5 +98,5 @@ pub fn chouse_move10(board: &mut Board) -> Option<Move> {
 }
 
 pub fn chouse_move20(board: &mut Board) -> Option<Move> {
-    best_move(board, 20)
+    best_move(board, 16)
 }
